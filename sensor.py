d0e2f2b79523e2b4cda0b8654d378b16fe06ee40
Alex <alex@example.com>
2021-10-12 20:50:29 +0200

    wip tests are passing with new sensor program
    need to split fixes from new feature
    need to figure out how to deploy it
    need to figure out what was wrong with npm

---
+++ sensor.py
@@ -0,0 +1,30 @@
+import time
+import os
+from package import sensor_timer
+
+
+def assert_equal(a, b):
+    assert a == b, "%s != %s" % (a, b)
+
+
+assert_equal(os.getenv("GPIOZERO_PIN_FACTORY"), "mock")
+
+# this package will test itself since importing here changes the path to the
+# sensor_timer import!
+__import__("package.sensor")
+# this works with a regular import too except flake8 doesn't like that it
+# is unused, and doesn't like that it's not at the top
+
+
+# let's make it take about one second
+c = int(1.0 / sensor_timer.delay)
+start_time = time.time()
+generator = sensor_timer.sensor_timer(1, 2, 3)
+for i in range(c):
+    next(generator)
+
+ellapsed_time = time.time() - start_time
+print(ellapsed_time)
+
+assert ellapsed_time > 0.5, ellapsed_time
+assert ellapsed_time < 1.5, ellapsed_time
