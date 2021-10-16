f63704070019d4f9043450a9eac6a855d94bf32d
alex <alex@example.com>
2021-10-16 18:52:29 +0200

    clean up be-quiet.sh
    remove old comment
    
    also it looks like we were activating venv for nothing
    so made it use `python` to take advantage

--- be-quiet.sh
+++ be-quiet.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-# a few little things that are easier to test straight in bash
-
 source venv/bin/activate
 
 set -o pipefail
 # make sure this program succeeds and has no output and test coverage should
 # take care of the rest
-venv/bin/python -u be_quiet.py 2>&1 | diff - /dev/null
+python -u be_quiet.py 2>&1 | diff - /dev/null
